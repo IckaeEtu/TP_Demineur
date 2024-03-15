252f5b176d71ca164c33dfb1edef1e71cb7cd2e8
EtuIckae <mickael.riego@etu.univ-orleans.fr>
2024-03-15 10:40:58 +0100

    Class CaseIntelligente added

--- src/CaseIntelligente.java
+++ src/CaseIntelligente.java
@@ -10,7 +10,39 @@ public class CaseIntelligente extends Case {
         this.lesVoisines = new ArrayList<>();
     }
 
+    /*Ajoute une case a la liste des cases voisines */
     public void ajouteVoisine(Case uneCase){
-        
+        this.lesVoisines.add(uneCase);
     }
+
+    /*Renvoie le nombre de cases voisines qui contienne une bombre */
+    public int nombreBombesVoisines(){
+        int cpt = 0;
+        for (Case uneCase : this.lesVoisines){
+            if (uneCase.contientUneBombe()){
+                cpt +=1;
+            }
+        }
+        return cpt;
+    }
+
+    /*Renvoie ce qui doit être affiché sur la case */
+    @Override
+    public String toString(){
+        if (this.estMarque){
+            return "?";
+        }
+        if (!(this.estDecouverte) && !(this.estMarque)){
+            return " ";
+        }
+        if (this.contientUneBombe) {
+            return "@";
+        }
+        else{
+            return String.valueOf(this.nombreBombesVoisines());
+        }
+
+    }
+
+
 }
